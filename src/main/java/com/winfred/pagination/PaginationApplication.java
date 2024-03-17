884c60574239b7fd90f70e412bf3f45cf9e7b907
winfred <achiengwinfred20@gmail.com>
2024-03-17 19:13:09 +0300

    java faker

--- src/main/java/com/winfred/pagination/PaginationApplication.java
+++ src/main/java/com/winfred/pagination/PaginationApplication.java
@@ -1,13 +1,20 @@
 package com.winfred.pagination;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaginationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaginationApplication.class, args);
+    }
+
+    @Bean
+    Faker faker(){
+        return new Faker();
     }
 
 }
--- src/main/java/com/winfred/pagination/data/SampleDataLoader.java
+++ src/main/java/com/winfred/pagination/data/SampleDataLoader.java
@@ -1,5 +1,6 @@
 package com.winfred.pagination.data;
 
+import com.github.javafaker.Faker;
 import com.winfred.pagination.model.Address;
 import com.winfred.pagination.model.Person;
 import com.winfred.pagination.repository.PersonRepository;
@@ -9,32 +10,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 
 @Component
 public class SampleDataLoader implements CommandLineRunner {
 
     @Autowired
     private final PersonRepository personRepository;
+    @Autowired
+    private final Faker faker;
     private Logger logger = LoggerFactory.getLogger(SampleDataLoader.class);
 
-    public SampleDataLoader(PersonRepository personRepository) {
+    public SampleDataLoader(PersonRepository personRepository, Faker faker ) {
         this.personRepository = personRepository;
+        this.faker = faker;
     }
 
     @Override
     public void run(String... args) throws Exception {
        logger.info("Loading sample data....");
 
-       Person person =new Person("Ned",
-               "Vega",
-               "0876532224",
-               "ned.vega@gmail.com",
-               new Address("Streeet",
-                       "city",
-                       "state",
-                       "zip"));
+        List<Person> people = IntStream.rangeClosed(1,100)
+                .mapToObj(i -> new Person(
+                        faker.name().firstName(),
+                        faker.name().lastName(),
+                        faker.phoneNumber().cellPhone(),
+                        faker.internet().emailAddress(),new Address(
+                                faker.address().streetAddress(),
+                                faker.address().city(),
+                                faker.address().state(),
+                                faker.address().zipCode()
+                )
 
-       personRepository.save(person);
+                ))
+                .toList();
 
+        personRepository.saveAll(people);
     }
 }
